ef0e8292b9c9b85e4d405d1d6e333ea796f1a855
xkollar3 <xkollar3@fi.muni.cz>
2024-04-04 16:28:54 +0200

    added mvc test, added swagger annotations

--- currency-service/src/main/java/cz/muni/fi/obs/controller/CurrencyController.java
+++ currency-service/src/main/java/cz/muni/fi/obs/controller/CurrencyController.java
@@ -4,12 +4,15 @@ import cz.muni.fi.obs.dto.CurrencyDto;
 import cz.muni.fi.obs.dto.CurrencyExchangeRequest;
 import cz.muni.fi.obs.dto.CurrencyExchangeResult;
 import cz.muni.fi.obs.facade.CurrencyFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Currency API")
 @RestController
 @RequestMapping("/v1/currencies")
 public class CurrencyController {
@@ -21,13 +24,16 @@ public class CurrencyController {
         this.currencyFacade = currencyFacade;
     }
 
+    @Operation(summary = "Exchanges sum between two specified currencies.")
     @PostMapping("/exchange")
     public ResponseEntity<CurrencyExchangeResult> exchange(@RequestBody CurrencyExchangeRequest request) {
         return ResponseEntity.ok(currencyFacade.exchange(request.from(), request.to(), request.amount()));
     }
 
-    @GetMapping("/")
-    public ResponseEntity<Page<CurrencyDto>> currencies(@ModelAttribute Pageable pageRequest) {
-        return ResponseEntity.ok(currencyFacade.listPage(pageRequest));
+    @Operation(summary = "Lists a page of available currencies to use")
+    @GetMapping("/list")
+    public ResponseEntity<Page<CurrencyDto>> currencies(@RequestParam(defaultValue = "0") Integer page,
+                                                        @RequestParam(defaultValue = "50") Integer pageSize) {
+        return ResponseEntity.ok(currencyFacade.listPage(Pageable.ofSize(pageSize).withPage(page)));
     }
 }
--- currency-service/src/main/java/cz/muni/fi/obs/dto/CurrencyExchangeRequest.java
+++ currency-service/src/main/java/cz/muni/fi/obs/dto/CurrencyExchangeRequest.java
@@ -1,6 +1,12 @@
 package cz.muni.fi.obs.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
+
 import java.math.BigDecimal;
 
-public record CurrencyExchangeRequest(String from, String to, BigDecimal amount) {
+@Validated
+public record CurrencyExchangeRequest(@NotBlank String from, @NotBlank String to, @NotNull @Min(0) BigDecimal amount) {
 }
