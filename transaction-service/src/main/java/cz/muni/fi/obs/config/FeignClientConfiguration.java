ead5b5fd0a19f7fb60e902db98aa4e325a3f069e
Filip Fabry <filip.fabry@wandera.com>
2024-04-14 13:26:21 +0200

    Restructure the Feign client configuration

--- transaction-service/src/main/java/cz/muni/fi/obs/config/FeignClientConfiguration.java
+++ transaction-service/src/main/java/cz/muni/fi/obs/config/FeignClientConfiguration.java
@@ -1,17 +1,10 @@
 package cz.muni.fi.obs.config;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import cz.muni.fi.obs.http.CurrencyServiceClient;
 
 public class FeignClientConfiguration {
-
-	@Configuration
-	@EnableFeignClients(basePackages = "cz.muni.fi.obs.http")
-	public static class FeignConfiguration {
-	}
 
 	public static class CurrencyServiceClientConfiguration {
 
