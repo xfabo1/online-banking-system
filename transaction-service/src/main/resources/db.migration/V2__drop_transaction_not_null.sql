654b8e2000add44dec2f162a63896b3375f39368
xkollar3 <xkollar3@fi.muni.cz>
2024-05-01 17:04:27 +0200

    fixed column types

--- transaction-service/src/main/resources/db.migration/V2__drop_transaction_not_null.sql
+++ transaction-service/src/main/resources/db.migration/V2__drop_transaction_not_null.sql
@@ -2,3 +2,7 @@ ALTER TABLE transactions
     ALTER COLUMN deposited_amount drop not null;
 ALTER TABLE transactions
     ALTER COLUMN conversion_rate drop not null;
+ALTER TABLE transactions
+    ALTER COLUMN note drop not null;
+ALTER TABLE transactions
+    ALTER COLUMN variable_symbol drop not null;
