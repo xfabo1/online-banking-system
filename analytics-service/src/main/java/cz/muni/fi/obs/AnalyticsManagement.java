0930ca46043b9e18ecd20ebea86fb626a89ef3e3
Vilem Gottwald <xvigo.dev@gmail.com>
2024-05-08 01:20:54 +0200

    analytics service security

--- analytics-service/src/main/java/cz/muni/fi/obs/AnalyticsManagement.java
+++ analytics-service/src/main/java/cz/muni/fi/obs/AnalyticsManagement.java
@@ -1,9 +1,16 @@
 package cz.muni.fi.obs;
 
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -12,7 +19,34 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 public class AnalyticsManagement {
 
+    public static final String SECURITY_SCHEME_BEARER = "Bearer";
+
+
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsManagement.class, args);
+    }
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(x -> x
+                        .requestMatchers(HttpMethod.GET, "/api/**").hasAuthority("SCOPE_test_read")
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken(Customizer.withDefaults()))
+        ;
+        return http.build();
+    }
+
+    @Bean
+    public OpenApiCustomizer openAPICustomizer() {
+        return openApi -> openApi.getComponents()
+                                 .addSecuritySchemes(SECURITY_SCHEME_BEARER,
+                                                     new SecurityScheme()
+                                                             .type(SecurityScheme.Type.HTTP)
+                                                             .scheme("bearer")
+                                                             .description("Provide an access token")
+                                 );
     }
 }
--- analytics-service/src/test/java/cz/muni/fi/obs/unit/controller/AnalyticsControllerTest.java
+++ analytics-service/src/test/java/cz/muni/fi/obs/unit/controller/AnalyticsControllerTest.java
@@ -8,15 +8,19 @@ import cz.muni.fi.obs.api.MonthlySummaryRequest;
 import cz.muni.fi.obs.api.MonthlySummaryResult;
 import cz.muni.fi.obs.controller.AnalyticsController;
 import cz.muni.fi.obs.facade.AnalyticsFacade;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -24,6 +28,7 @@ import java.util.ArrayList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,60 +37,71 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = {Application.class})
 class AnalyticsControllerTest {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
     @MockBean
     private AnalyticsFacade analyticsFacade;
-
     @Autowired
+    private WebApplicationContext context;
     private MockMvc mockMvc;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
+    @WithMockUser(username = "test", authorities = {"SCOPE_test_read"})
     @Test
     void getDailySummary_validRequest_returnsASummary() throws Exception {
         when(analyticsFacade.getDailySummary(any(String.class), any(Integer.class), any(Integer.class)))
                 .thenReturn(new DailySummaryResult(LocalDate.now(), new ArrayList<>()));
 
         mockMvc.perform(post("/v1/12345/daily-summary")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new DailySummaryRequest(2021, 1))))
-                .andExpect(status().isOk());
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new DailySummaryRequest(2021, 1))))
+               .andExpect(status().isOk());
 
         verify(analyticsFacade).getDailySummary("12345", 2021, 1);
     }
 
+    @WithMockUser(username = "test", authorities = {"SCOPE_test_read"})
     @Test
     void getDailySummary_badRequest_throwsException() throws Exception {
         when(analyticsFacade.getDailySummary(any(String.class), any(Integer.class), any(Integer.class)))
                 .thenReturn(new DailySummaryResult(LocalDate.now(), new ArrayList<>()));
 
         mockMvc.perform(post("/v1/12345/daily-summary")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new DailySummaryRequest(2021, -1))))
-                .andExpect(status().isBadRequest());
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new DailySummaryRequest(2021, -1))))
+               .andExpect(status().isBadRequest());
 
     }
 
+    @WithMockUser(username = "test", authorities = {"SCOPE_test_read"})
     @Test
     void getMonthlySummary_validRequest_returnsASummary() throws Exception {
         when(analyticsFacade.getMonthlySummary(any(String.class), any(Integer.class), any(Integer.class)))
                 .thenReturn(new MonthlySummaryResult(LocalDate.now(), null));
 
         mockMvc.perform(post("/v1/12345/monthly-summary")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new MonthlySummaryRequest(2021, 1))))
-                .andExpect(status().isOk());
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new MonthlySummaryRequest(2021, 1))))
+               .andExpect(status().isOk());
 
         verify(analyticsFacade).getMonthlySummary("12345", 2021, 1);
     }
 
+    @WithMockUser(username = "test", authorities = {"SCOPE_test_read"})
     @Test
     void getMonthlySummary_badRequest_throwsException() throws Exception {
         when(analyticsFacade.getMonthlySummary(any(String.class), any(Integer.class), any(Integer.class)))
                 .thenReturn(new MonthlySummaryResult(LocalDate.now(), null));
 
         mockMvc.perform(post("/v1/12345/monthly-summary")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new MonthlySummaryRequest(-10, 1))))
-                .andExpect(status().isBadRequest());
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(new MonthlySummaryRequest(-10, 1))))
+               .andExpect(status().isBadRequest());
     }
 }
